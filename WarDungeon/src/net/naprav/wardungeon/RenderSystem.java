ff077e3786103817396f13a8302247672fad26f3
Nikola Grgic <nikola.grgic.4@gmail.com>
2013-09-27 18:41:32 -0400

    Made some random color stuffs! :D

--- WarDungeon/src/net/naprav/wardungeon/RenderSystem.java
+++ WarDungeon/src/net/naprav/wardungeon/RenderSystem.java
@@ -1,9 +1,15 @@
 package net.naprav.wardungeon;
 
+import java.util.Random;
+
 public class RenderSystem {
 
 	private int width, height;
 	public int[] allPixels;
+
+	public int[] block = new int[64 * 64];
+	public int blockSize = 16;
+	private Random random = new Random();
 
 	/**
 	 * The constructor for the RenderMechanism class.
@@ -15,6 +21,10 @@ public class RenderSystem {
 		this.width = width;
 		this.height = height;
 		allPixels = new int[width * height];
+
+		for (int i = 0; i < block.length; i++) {
+			block[i] = random.nextInt(0xFFFFFF);
+		}
 	}
 
 	/**
@@ -22,8 +32,18 @@ public class RenderSystem {
 	 */
 	public void changePixels() {
 		for (int x = 0; x < this.width; x++) {
+			int xMove = x;
+			if (xMove < 0 || xMove >= width) {
+				break;
+			}
 			for (int y = 0; y < this.height; y++) {
-				allPixels[x + (y * width)] = 0x23CF8A;
+				int yMove = y;
+				if (yMove < 0 || yMove >= height) {
+					break;
+				}
+				//The "& 63" refers to when we get to 63, go back to 0, thus looping.
+				int blockIndex = ((xMove / blockSize) & 63) + ((yMove / blockSize) & 63) * 64;
+				allPixels[x + (y * width)] = block[blockIndex];
 			}
 		}
 	}
